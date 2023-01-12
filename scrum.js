685a456ee65eedb4dec3595898df0da8d5b25c1c
Noereynoso <rnoe23462@gmail.com>
2023-01-12 11:44:08 -0300

    cosas hechas

---
+++ scrum.js
@@ -0,0 +1,22 @@
+/*
+Scrum
+
++ Marco de trabajo
++ usa algunos conociminetos de manifiesto agil 
+
+1. Perfiles
+
+____
+
+*product owner || PO: _"Quien se comunica con el Cliente, tambien realiza las *_(historias de los usarios)_*"_ 
++scrum Master  || SM: _" es un facilitador 
+Proyect de Manger || PH:
+Equipo de desarrollo:
+    + Disiñadores 
+    + Programadores
+    + Productores del contenido 
++ stakeholder || consumidores || testers:
+
+
++
+*/
